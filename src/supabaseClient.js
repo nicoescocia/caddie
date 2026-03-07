7711565846f1d31d4a21ddb1805deb59b380f2b5
Nico <nico@bonusbank.com.au>
2026-03-07 20:11:58 +0000

    Use env vars for Supabase client

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://xyruyxfcwxhdyzvrzxqg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5cnV5eGZjd3hoZHl6dnJ6eHFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NDU3MDUsImV4cCI6MjA4ODMyMTcwNX0.TRimQGamjH66-LcDK3HKvFWXmUVIbpfr9Lx5Wt7ZWz4'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
